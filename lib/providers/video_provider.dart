41a37f80e8eeed69f59602184db3a53cbab6bb0a
denos <costelasdenis@gmail.com>
2023-05-07 15:59:21 +0300

    Gets the videos links

--- lib/providers/video_provider.dart
+++ lib/providers/video_provider.dart
@@ -1,31 +1,28 @@
 import 'dart:io';
-import 'dart:io' show Platform;
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as parser;
 
 class VideoProvider with ChangeNotifier {
+  // ignore: prefer_final_fields
+  List<String> _videoList = [];
+
+  List<String> get videos {
+    return [..._videoList];
+  }
+
   Future<void> getVideosFromPH() async {
     Uri url = Uri.parse('https://www.pornhub.com/');
     try {
       final response = await http.get(url);
       var htmlDocument = parser.parse(response.body);
-      final mp4Urls =
-          htmlDocument.querySelectorAll('video.js-gifVideoBlock source.js-mp4');
-      // test folder
-      final directory =
-          Directory('${Platform.environment['HOME']}/my_directory');
-      await directory.create(
-          recursive: true); // Create the directory if it doesn't exist
-
-      final file = File(
-          '${directory.path}/my_file.txt'); // The file you want to save the string to
-      await file.writeAsString(response.body); // Save the string to the file
-      // test gata
+      final mp4Urls = htmlDocument.querySelectorAll('video');
       for (final mp4Url in mp4Urls) {
-        final url = mp4Url.attributes['src'];
-        print(url); // prints the URL of the .mp4 file
+        final url = mp4Url.attributes['data-mp4'];
+        if (url != null) {
+          _videoList.add(url);
+        }
       }
     } catch (e) {
       print(e);
